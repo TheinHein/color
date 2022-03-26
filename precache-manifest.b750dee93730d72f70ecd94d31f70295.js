e641ae53934006f097bd49232d1b2429a2ee575d
Thein Than Hein <thein.than.hein.th@gmail.com>
2022-03-25 20:52:44 -0700

    Updates

---
+++ precache-manifest.b750dee93730d72f70ecd94d31f70295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cce40e74df7717b7ca0d",
+    "url": "/color/static/css/main.0e38210e.chunk.css"
+  },
+  {
+    "revision": "cce40e74df7717b7ca0d",
+    "url": "/color/static/js/main.cce40e74.chunk.js"
+  },
+  {
+    "revision": "a78f8d9d3a2c5687ad6b",
+    "url": "/color/static/js/1.a78f8d9d.chunk.js"
+  },
+  {
+    "revision": "2efa125cdc557556a86e",
+    "url": "/color/static/js/2.2efa125c.chunk.js"
+  },
+  {
+    "revision": "7132b75db203b18dd94b",
+    "url": "/color/static/js/runtime~main.7132b75d.js"
+  },
+  {
+    "revision": "c8866b8300ad5f04f9de6e220fc7eedf",
+    "url": "/color/index.html"
+  }
+];
